3e4a78750606d58ee59a7098fd2966087741d6ec
Shah <armanshah360@gmail.com>
2019-09-05 14:43:11 -0700

    Implement action to reset redux state on login

---
+++ src/actions/reset.js
@@ -0,0 +1,18 @@
+import {
+  RESET_JAR,
+  RESET_ACCOUNT_STATE,
+  CLEAR_USER_DATA,
+  RESET_CHARACTER_STATE,
+  CLEAR_ACTIVE_PET,
+  CLEAR_GAME_DATA
+} from '../constants/types';
+
+// Reset Redux State
+export default () => dispatch => {
+  dispatch({ type: RESET_JAR });
+  dispatch({ type: RESET_ACCOUNT_STATE });
+  dispatch({ type: CLEAR_USER_DATA });
+  dispatch({ type: RESET_CHARACTER_STATE });
+  dispatch({ type: CLEAR_ACTIVE_PET });
+  dispatch({ type: CLEAR_GAME_DATA });
+};
