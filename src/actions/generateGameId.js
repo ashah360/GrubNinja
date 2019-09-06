cd1e1e96aefb7f5e0f9972c77da48ff415c36795
Arman <armanshah360@gmail.com>
2019-09-05 21:38:46 -0700

    Implement action for generating game session

---
+++ src/actions/generateGameId.js
@@ -0,0 +1,38 @@
+import parser from 'fast-xml-parser';
+import store from '../store';
+import { START_MINIGAME } from '../constants/endpoints';
+import commonForm from '../constants/minigame';
+import { SET_GAME_ID } from '../constants/types';
+
+// Generate game session with current game state from redux store
+export const generateGameId = () => async (dispatch, getState) => {
+  const { game } = getState();
+
+  try {
+    let body = await window.request({
+      method: 'POST',
+      uri: START_MINIGAME,
+      form: { ...game, ...commonForm }
+    });
+
+    let gameData = parser.parse(body, {
+      parseNodeValue: false
+    }).StartMinigameResponse;
+
+    if (gameData.Status.Msg == 'Success') {
+      dispatch({
+        type: SET_GAME_ID,
+        payload: gameData.CurrentGameId
+      });
+
+      return Promise.resolve();
+    } else {
+      let errorMessage = `${gameData.Status.Msg} - ${gameData.Status.Content}`;
+      console.error(errorMessage);
+      return Promise.reject(errorMessage);
+    }
+  } catch (error) {
+    console.error(error);
+    return Promise.resolve(store.dispatch(generateGameId()));
+  }
+};
