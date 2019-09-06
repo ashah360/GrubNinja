0375f98246dd75030863efa5de618f9d8be9129e
Arman <armanshah360@gmail.com>
2019-09-05 21:39:42 -0700

    Set request defaults

--- preload.js
+++ preload.js
@@ -1,2 +1,8 @@
+const https = require('https');
+const request = require('request-promise');
+
+const agent = new https.Agent({ keepAlive: true });
+const jar = request.jar();
+
 window.ipcRenderer = require('electron').ipcRenderer;
-window.request = require('request-promise');
+window.request = request.defaults({ agent, jar });
