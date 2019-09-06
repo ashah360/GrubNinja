702ed1336082b26fa902b6f490bf0136c9284355
Arman <armanshah360@gmail.com>
2019-09-05 21:41:27 -0700

    Map constants

--- src/constants/maps.js
+++ src/constants/maps.js
@@ -1,1 +1,61 @@
 export const DEFAULT_MAP = 'MAP-WC-001';
+
+export const coreMaps = [
+  { value: 'MAP-WC-001', label: '[WC] Unicorn Way' },
+  { value: 'MAP-WC-002', label: '[WC] Firecat Alley' },
+  { value: 'MAP-WC-003', label: '[WC] Triton Avenue' },
+  { value: 'MAP-WC-004', label: '[WC] Colossus Blvd' },
+  { value: 'MAP-KT-001', label: '[KT] The Oasis' },
+  { value: 'MAP-KT-002', label: '[KT] Tomb of Storms' },
+  { value: 'MAP-KT-003', label: '[KT] Pyramid of the Sun' },
+  { value: 'MAP-KT-004', label: '[KT] The Krokosphinx' },
+  { value: 'MAP-MB-001', label: '[MB] Hyde Park' },
+  { value: 'MAP-MB-002', label: '[MB] Chelsea Court' },
+  { value: 'MAP-MB-003', label: '[MB] The Ironworks' },
+  { value: 'MAP-MB-004', label: '[MB] Kensington Park' },
+  { value: 'MAP-MS-001', label: '[MS] Shiritaki Temple' },
+  { value: 'MAP-MS-002', label: '[MS] Village of Sorrow' },
+  { value: 'MAP-MS-003', label: '[MS] The Tree of Life' },
+  { value: 'MAP-MS-004', label: '[MS] The Jade Palace' },
+  { value: 'MAP-DS-001', label: '[DS] The Labyrinth' },
+  { value: 'MAP-DS-002', label: '[DS] The Grand Chasm' },
+  { value: 'MAP-DS-003', label: '[DS] The Basilica' },
+  { value: 'MAP-DS-004', label: '[DS] The Crown of Fire' },
+  { value: 'MAP-CL-001', label: '[CL] Survey Camp' },
+  { value: 'MAP-CL-002', label: '[CL] Floating Lands' },
+  { value: 'MAP-CL-003', label: '[CL] Stormriven' },
+  { value: 'MAP-CL-004', label: '[CL] Trial of the Spheres' }
+];
+
+export const premiumMaps = {
+  'GG-WORLD-AV': [
+    { value: 'MAP-AV-001', label: '[AV] Caliburn' },
+    { value: 'MAP-AV-002', label: '[AV] Lake Nimue' },
+    { value: 'MAP-AV-003', label: '[AV] The Weirwood' },
+    { value: 'MAP-AV-004', label: '[AV] Castle Avalon' }
+  ],
+  'GG-WORLD-AZ': [
+    { value: 'MAP-AZ-001', label: '[AZ] Mangrove Marsh' },
+    { value: 'MAP-AZ-002', label: '[AZ] Saltmeadow Swamp' },
+    { value: 'MAP-AZ-003', label: '[AZ] Cloudburst Forest' },
+    { value: 'MAP-AZ-004', label: '[AZ] Alto Alto' }
+  ],
+  'GG-WORLD-PA': [
+    { value: 'MAP-PA-001', label: '[PA] Pigswick Academy' },
+    { value: 'MAP-PA-002', label: '[PA] Pegasus Place' },
+    { value: 'MAP-PA-003', label: '[PA] Tanglewood Way' },
+    { value: 'MAP-PA-004', label: '[PA] Helephant Tower' }
+  ],
+  'GG-WORLD-TU': [
+    { value: 'MAP-TU-001', label: '[TU] Crab Alley' },
+    { value: 'MAP-TU-002', label: '[TU] Deep Warrens' },
+    { value: 'MAP-TU-003', label: '[TU] Wailing Caverns' },
+    { value: 'MAP-TU-004', label: '[TU] The Waterworks' }
+  ],
+  'GG-WORLD-ZF': [
+    { value: 'MAP-ZF-001', label: '[ZF] Zamunda' },
+    { value: 'MAP-ZF-002', label: '[ZF] Stone Town' },
+    { value: 'MAP-ZF-003', label: '[ZF] Drum Jungle' },
+    { value: 'MAP-ZF-004', label: '[ZF] Mirror Lake' }
+  ]
+};
