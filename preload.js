3cb3b27dbb8696d72f1ed451dbbb5a1a895f650c
Shah <armanshah360@gmail.com>
2019-09-13 15:10:33 -0700

    remove https agent to prevent socket hangups

--- preload.js
+++ preload.js
@@ -1,12 +1,12 @@
 const https = require('https');
 const request = require('request-promise');
 
-const agent = new https.Agent({ keepAlive: true });
+//const agent = new https.Agent({ keepAlive: true });
 const jar = request.jar();
 
 window.ipcRenderer = require('electron').ipcRenderer;
 window.request = request.defaults({
-  agent,
+  //agent,
   jar,
   rejectUnauthorized: false, // ignore self-signed certs because KI is garbage
   insecure: true
