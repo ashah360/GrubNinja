75b72ef70c74ae4a8f0e803e3ab49598671e67e6
Shah <armanshah360@gmail.com>
2019-09-13 15:16:08 -0700

    Add quick map selection

--- src/components/trainer/subcomponents/TrainerControl.jsx
+++ src/components/trainer/subcomponents/TrainerControl.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import setMap from '../../../actions/setMap';
 import PropTypes from 'prop-types';
 
 const TrainerControl = props => {
@@ -10,62 +13,54 @@ const TrainerControl = props => {
             <div className='row mb-3'>
               <div className='col'>
                 <h5 className='card-title text-uppercase text-muted mb-0'>
-                  Auto Trainer Options
+                  Map Quick Select
                 </h5>
               </div>
             </div>
-            <div className='row'>
-              <div className='col trainer-control'>
-                <div className='row'>
-                  <div className='col'>
-                    <div className='custom-control custom-checkbox mb-3'>
-                      <input
-                        className='auto-trainer-input custom-control-input'
-                        type='checkbox'
-                        id='enable-auto-trainer'
-                      />
-                      <label
-                        className='custom-control-label'
-                        htmlFor='enable-auto-trainer'
-                      >
-                        Enable Auto Trainer
-                      </label>
-                    </div>
-                  </div>
+            <div className='form-row mb-3 preset-group form'>
+              <div className='col'>
+                <div className='custom-control custom-radio mb-3'>
+                  <input
+                    name='preset-group'
+                    className='custom-control-input preset-group'
+                    id='no-preset'
+                    type='radio'
+                    defaultChecked={true}
+                  />
+                  <label className='custom-control-label' htmlFor='no-preset'>
+                    No Preset
+                  </label>
                 </div>
-                <div className='row'>
-                  <div className='col'>
-                    <div className='custom-control custom-checkbox mb-3'>
-                      <input
-                        className='auto-refill-energy custom-control-input'
-                        type='checkbox'
-                        id='auto-refill-energy'
-                      />
-                      <label
-                        className='custom-control-label'
-                        htmlFor='auto-refill-energy'
-                      >
-                        Automatically refill energy
-                      </label>
-                    </div>
-                  </div>
+                <div className='custom-control custom-radio mb-3'>
+                  <input
+                    name='preset-group'
+                    className='custom-control-input preset-group'
+                    id='tots-preset'
+                    type='radio'
+                    onClick={() => props.setMap('MAP-CL-004')}
+                  />
+                  <label className='custom-control-label' htmlFor='tots-preset'>
+                    Free Maps (12 XP)
+                  </label>
                 </div>
-                <div className='row'>
-                  <div className='col'>
-                    <div className='custom-control custom-checkbox'>
-                      <input
-                        className='train-next-level custom-control-input'
-                        type='checkbox'
-                        id='train-next-level'
-                      />
-                      <label
-                        className='custom-control-label'
-                        htmlFor='train-next-level'
-                      >
-                        Train until next level
-                      </label>
-                    </div>
-                  </div>
+                <div className='custom-control custom-radio'>
+                  <input
+                    name='preset-group'
+                    className='custom-control-input preset-group'
+                    id='wondra-preset'
+                    type='radio'
+                    onClick={() =>
+                      props.setMap(
+                        _.sample(['MAP-AV-004', 'MAP-AZ-003', 'MAP-AZ-004'])
+                      )
+                    }
+                  />
+                  <label
+                    className='custom-control-label'
+                    htmlFor='wondra-preset'
+                  >
+                    Premium Maps (16 XP)
+                  </label>
                 </div>
               </div>
             </div>
@@ -76,6 +71,11 @@ const TrainerControl = props => {
   );
 };
 
-TrainerControl.propTypes = {};
+TrainerControl.propTypes = {
+  setMap: PropTypes.func.isRequired
+};
 
-export default TrainerControl;
+export default connect(
+  null,
+  { setMap }
+)(TrainerControl);
