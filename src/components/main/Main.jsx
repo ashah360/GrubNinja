d54e0af783de2b3674cda1ec8808721ab69aba48
Arman <armanshah360@gmail.com>
2019-09-12 23:31:57 -0700

    Begin implementing saved account data

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,15 +2,22 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import UserDropdown from '../misc/UserDropdown';
 import { loadMetrics } from '../../actions/loadMetrics';
+import { setAccountDetails } from '../../actions/character';
 
 const Main = props => {
   useEffect(() => {
     window.ipcRenderer.send('request-metrics');
+    window.ipcRenderer.send('request-saved-account');
   }, []);
 
   // On metrics event, update our state
   window.ipcRenderer.on('load-metrics', (e, payload) => {
     props.loadMetrics(payload);
+  });
+
+  // Same with username
+  window.ipcRenderer.on('load-saved-account', (e, payload) => {
+    props.setAccountDetails(payload.username, payload.password);
   });
 
   return (
@@ -22,5 +29,5 @@ const Main = props => {
 
 export default connect(
   null,
-  { loadMetrics }
+  { loadMetrics, setAccountDetails }
 )(Main);
